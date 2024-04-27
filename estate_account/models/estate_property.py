daa374b2d36b836d3f06446ab27534ec544c5187
daniela.rivera <danielarivera@gmail.com>
2024-04-27 11:48:25 -0400

    modificaciones en la funcion action_vender

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,20 +1,20 @@
 from odoo import fields, models, api
+from odoo.fields import Command
 
 
 class Property(models.Model):
     _inherit = 'estate.property'
 
     def action_vender(self):
-        invoice = self.env['account.move'].create({
+        self.env['account.move'].create({
             'partner_id': self.buyer.id,
             'move_type': 'out_invoice',
-        })
-
-        self.env['account.move.line'].create({
-            'move_id': invoice.id,
-            'name': self.name,
-            'quantity': 1,
-            'price_unit': self.sale_price * 0.06 + 100,
+            'invoice_line_ids': [
+                Command.create({'name': self.name,
+                                'quantity': 1,
+                                'partner_id': self.buyer.id,
+                                'price_unit': self.sale_price * 0.06 + 100}),
+            ],
         })
 
         return super().action_vender()
