6273a2c8a70f55dfc4e2f433ada7cb093b9d7ef9
daniela.rivera <danielarivera@gmail.com>
2024-04-27 11:13:25 -0400

    creando una factura con lineas de pedido a partir de un boton

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,21 +1,20 @@
 from odoo import fields, models, api
-from odoo import Command
 
 
 class Property(models.Model):
     _inherit = 'estate.property'
 
     def action_vender(self):
-        self.env['account.move'].create({
+        invoice = self.env['account.move'].create({
             'partner_id': self.buyer.id,
             'move_type': 'out_invoice',
-            'lines_id': [Command.create({
-                "field_1": "value_1",
-                "field_2": "value_2",
-
-            })]
-
         })
+
+        self.env['account.move.line'].create({
+            'move_id': invoice.id,
+            'name': self.name,
+            'quantity': 1,
+            'price_unit': self.sale_price * 0.06 + 100,
+        })
+
         return super().action_vender()
-
-
