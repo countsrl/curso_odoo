09a16fab04f280282316fff5f8b8fe8212a7edcc
unknown <rlvelazquez86@gmail.com>
2024-04-27 11:23:39 -0500

    working inverse function date_deadline

--- real__estate/models/estate_property_offer.py
+++ real__estate/models/estate_property_offer.py
@@ -4,6 +4,9 @@ import logging
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
+from datetime import timedelta, date
+
+
 _logger = logging.getLogger(__name__)
 
 
@@ -21,8 +24,24 @@ class Estate_property_offer(models.Model):
         copy=False,
         default= "accepted",)
       
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(string= "Dead Line", compute='_compute_date_deadline' , inverse='_inverse_date_deadline', store=True, readonly=True)
+
+
+    create_date = fields.Date(default=lambda self: fields.Date.today())     
     partner_id = fields.Many2one("res.partner",string = "Partner",required=True)  
     property_id = fields.Many2one("estate.property",string = "Property", required=True)
+    
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:  # Verifica si create_date tiene un valor
+                record.validity = (record.date_deadline - record.create_date).days
     
     
         
