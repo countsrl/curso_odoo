88d091d9db5712f3c3a61c0c40d9b65bc2afb4c0
daniela.rivera <danielarivera@gmail.com>
2024-05-20 20:48:50 -0400

    agregando modulo para gestionar hostal

---
+++ management_hotel/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Management Hotel",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Daniela Rivera Feria",
+    'website': " ",
+    'application': True,
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+
+    ],
+
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
