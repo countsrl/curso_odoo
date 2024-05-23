4872c413308d64927ec1c49c9d09e2a1024ba73b
daniela.rivera <danielarivera@gmail.com>
2024-05-22 21:55:42 -0400

    sequencia corregida

--- hostel_management/models/reservation.py
+++ hostel_management/models/reservation.py
@@ -6,8 +6,8 @@ from odoo import fields, models, api, _
 class Reservations(models.Model):
     _name = 'reservation'
 
-    sequence = fields.Char(default=lambda self: _('New'))
-    id_reservation = fields.Char('Reservation ID', required=True)
+    sequence = fields.Char(string='Sequence')
+    id_reservation = fields.Char('Reservation ID', default=lambda self: _('New'), readonly=True)
     client_id = fields.Many2one('res.partner', 'Client name', reuired=True)
     room_no = fields.Integer('Room No.', required=True)
     init_date = fields.Date('Init Date', required=True)
@@ -18,21 +18,29 @@ class Reservations(models.Model):
                                       'Method of Payment', required=True)
     notes = fields.Text('Notes')
     service_ids = fields.Many2many('service')
-    states = fields.Selection(
-        ([('confirm', 'Confirm'), ('canceled', 'Canceled'), ('new', 'New'), ('finished', 'Finished'),
-          ('invoice', 'Invoice'), ('invoiced', 'Invoiced')]), 'States', default='new')
+    states = fields.Selection(([('new', 'New'), ('confirm', 'Confirm'),
+                                ('invoice', 'Invoice'), ('invoiced', 'Invoiced'), ('finished', 'Finished'),
+                                ('canceled', 'Canceled')]), 'States', default='new')
 
     def action_cancelar(self):
         self.write({'states': 'canceled'})
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.id_reservation
+            result.append((record.id, name))
+        return result
 
     @api.model
     def create(self, vals):
         request = super(Reservations, self).create(vals)
 
         if vals.get('sequence', _('New')) == _('New'):
-            vals['sequence'] = self.env['ir.sequence'].next_by_code('hostel_management.reservation.seq') or _('New')
+            vals['sequence'] = self.env['ir.sequence'].next_by_code('reservation') or _('New')
             aux = 'R'
             date = datetime.now().year
             request.id_reservation = '{0}-{1}/{2}'.format(aux, vals['sequence'], date)
 
-        return super(Reservations, self).write(vals)
+        request.write(vals)
+        return request
